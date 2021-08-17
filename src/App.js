e5e1c3ddf15b277eed03c703cc08ade1130bb4ae
Erzhan2101 <erzhan210198@gmail.com>
2021-08-17 21:36:17 +0600

    pro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './style.css'
 import Header from "./header";
+import Hero from "./Hero";
 
 function App() {
     return (
         <div>
             <Header />
+            <Hero />
         </div>
     );
 }
---
+++ src/Hero.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Hero() {
+    return (
+        <div className='hero'>
+            <div className='container'>
+                <div className="box-title">
+                    <h1 className="title">Shop</h1>
+                    <p className="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        <br/> tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Hero
